b5f6b1c4f84c1a2aedd18c6e5c2747eec7bd5f76
ranggacey <ranggadanceyong@gmail.com>
2025-02-13 13:54:13 +0700

    Update favicon, layout, dan data places

--- src/data/places.js
+++ src/data/places.js
@@ -1,8 +1,9 @@
 const places = [
-    { name: "Simpang Lima", lat: -6.9821, lng: 110.4108, category: "Wisata", img: "/simpang-lima.jpg" },
-    { name: "Lumpia Gang Lombok", lat: -6.9715, lng: 110.4223, category: "Kuliner", img: "/lumpia.jpg" },
-    { name: "Lawang Sewu", lat: -6.9835, lng: 110.4092, category: "Sejarah", img: "/lawangsewu.png" },
-    { name: "Kota Lama", lat: -6.9700, lng: 110.4288, category: "Wisata", img: "/kota-lama.jpg" },
+    { name: "Simpang Lima", lat: -6.989997, lng: 110.422279, category: "Wisata", img: "/simpang-lima.jpg" },
+    { name: "Lumpia Gang Lombok", lat: -6.974390, lng: 110.427954, category: "Kuliner", img: "/lumpia.jpg" },
+    { name: "Lawang Sewu", lat: -6.984082, lng: 110.410503, category: "Sejarah", img: "/lawangsewu.jpg" },
+    { name: "Kota Lama", lat: -6.968255, lng: 110.428432, category: "Wisata", img: "/kota-lama.jpg" },
+    { name: "Curug Gondoriyo", lat: -7.005240, lng: 110.321117, category: "Wisata Alam", img: "/gondoriyo.jpg" },
   ];
   
   export default places;
